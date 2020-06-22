87d4c8c3b69a1374e09f1b89fc48c73b49ffcf7f
DaliaZH <Dalia_zhran@hotmail.com>
2020-06-22 08:50:44 +0200

    marker click go to flatlist item and added hours selection with price

--- components/MapView.js
+++ components/MapView.js
@@ -7,16 +7,15 @@ import {
   TouchableWithoutFeedback,
   TouchableOpacity,
   FlatList,
-  // ScrollView,
 } from "react-native";
 import { Table, Row } from "react-native-table-component";
 import { ScrollView } from "react-native-gesture-handler";
 import MapView, { Marker, Callout } from "react-native-maps";
 import Modal from "react-native-modal";
+import Dropdown from "react-native-modal-dropdown";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
 import { Button } from "../components";
-import { markerTheme } from "../constants";
-import { theme } from "../constants";
+import { markerTheme, theme } from "../constants";
 
 const { height, width } = Dimensions.get("screen");
 const API = "http://192.168.1.17:8080/api/parking";
@@ -26,9 +25,28 @@ class MyMapView extends React.Component {
     active: null,
     activeModal: null,
     activeLots: [],
+    hours: {},
     lotsTableData: [],
     tableHead: ["Lot No.", "Status"],
     widthArr: [150, 150],
+  };
+
+  UNSAFE_componentWillMount() {
+    const { markers } = this.props;
+    const hours = {};
+
+    markers.map((parking) => {
+      hours[parking.UUID] = 1;
+    });
+
+    this.setState({ hours });
+  }
+
+  handleHours = (id, value) => {
+    const { hours } = this.state;
+    hours[id] = value;
+
+    this.setState({ hours });
   };
 
   getLots = (id) => {
@@ -54,11 +72,15 @@ class MyMapView extends React.Component {
         throw err;
       });
   };
+
   renderParking = (item) => {
+    const { hours } = this.state;
+    const totalPrice = parseInt(item.price) * hours[item.UUID];
+
     return (
       <TouchableWithoutFeedback
         key={`parking-${item.UUID}`}
-        onPress={() => this.setState({ active: item.UUID })}
+        // onPress={() => this.setState({ active: item.UUID })}
       >
         <View style={[styles.parking, styles.shadow]}>
           <View style={styles.hours}>
@@ -66,26 +88,25 @@ class MyMapView extends React.Component {
               x {item.spots} {item.name}
             </Text>
             <View style={{ flexDirection: "row", alignItems: "center" }}>
-              <Text style={{ color: markerTheme.COLORS.gray }}>
-                {item.address}
-              </Text>
+              {this.renderHours(item.UUID)}
+              <Text style={{ color: markerTheme.COLORS.gray }}>hrs</Text>
             </View>
           </View>
           <View style={styles.parkingInfoContainer}>
             <View style={styles.parkingInfo}>
               <View style={styles.parkingIcon}>
                 <Ionicons
-                  name="ios-car"
+                  name="md-pricetag"
                   size={markerTheme.SIZES.icon * 1.3}
                   color={markerTheme.COLORS.gray}
                 />
                 <Text style={{ marginLeft: markerTheme.SIZES.base }}>
-                  {item.spots}
+                  £{item.price}
                 </Text>
               </View>
               <View style={styles.parkingIcon}>
                 <Ionicons
-                  name="ios-checkmark-circle"
+                  name="md-checkmark-circle"
                   size={markerTheme.SIZES.icon * 1.3}
                   color={markerTheme.COLORS.green}
                 />
@@ -101,8 +122,16 @@ class MyMapView extends React.Component {
             >
               <View style={styles.buyTotal}>
                 <View style={{ flexDirection: "row", alignItems: "center" }}>
-                  <Text style={styles.buyTotalPrice}>View Details</Text>
+                  <FontAwesome
+                    name="gbp"
+                    size={markerTheme.SIZES.icon * 1.5}
+                    color={markerTheme.COLORS.white}
+                  />
+                  <Text style={styles.buyTotalPrice}>{totalPrice}</Text>
                 </View>
+                <Text style={styles.calculation}>
+                  {item.price}x{hours[item.UUID]} hrs
+                </Text>
               </View>
               <View style={styles.buyBtn}>
                 <FontAwesome
@@ -131,12 +160,19 @@ class MyMapView extends React.Component {
         data={this.props.markers}
         keyExtractor={(item, index) => `${item.UUID}`}
         renderItem={({ item }) => this.renderParking(item)}
+        ref={(ref) => {
+          this.flatListRef = ref;
+        }}
       />
     );
   };
+  scrollToIndex = (selectedIndex) => {
+    this.flatListRef.scrollToIndex({ animated: true, index: selectedIndex });
+  };
 
   renderModal() {
-    const activeModal = this.state.activeModal;
+    // const activeModal = this.state.activeModal;
+    const { activeModal, hours } = this.state;
     if (!activeModal) return null;
     this.getLots(activeModal.UUID);
     return (
@@ -175,13 +211,26 @@ class MyMapView extends React.Component {
               style={[styles.parkingIcon, { justifyContent: "flex-start" }]}
             >
               <Ionicons
-                name="ios-car"
+                name="md-car"
                 size={markerTheme.SIZES.icon * 1.6}
                 color={markerTheme.COLORS.gray}
               />
               <Text style={{ fontSize: markerTheme.SIZES.icon * 1.15 }}>
                 {" "}
                 {activeModal.free}/{activeModal.spots}
+              </Text>
+            </View>
+            <View
+              style={[styles.parkingIcon, { justifyContent: "flex-start" }]}
+            >
+              <Ionicons
+                name="md-pricetag"
+                size={markerTheme.SIZES.icon * 1.4}
+                color={markerTheme.COLORS.gray}
+              />
+              <Text style={{ fontSize: markerTheme.SIZES.icon * 1.15 }}>
+                {" "}
+                £{activeModal.price}
               </Text>
             </View>
           </View>
@@ -216,6 +265,23 @@ class MyMapView extends React.Component {
               </View>
             </ScrollView>
           </View>
+          <View style={styles.payBtnContainer}>
+            <Button
+              gradient
+              style={styles.payBtn}
+              // onPress={() => this.setState({ activeModal: item })}
+            >
+              <Text style={styles.payText}>
+                Proceed to pay £{activeModal.price * hours[activeModal.UUID]}
+                {"  "}
+              </Text>
+              <FontAwesome
+                name="angle-right"
+                size={markerTheme.SIZES.icon * 1.75}
+                color={markerTheme.COLORS.white}
+              />
+            </Button>
+          </View>
         </View>
       </Modal>
     );
@@ -236,6 +302,25 @@ class MyMapView extends React.Component {
         />
       );
     });
+  }
+  renderHours(id) {
+    const { hours } = this.state;
+    const availableHours = [1, 2, 3, 4, 5, 6];
+
+    return (
+      <Dropdown
+        defaultIndex={0}
+        options={availableHours}
+        style={styles.hoursDropdown}
+        defaultValue={`0${hours[id]}:00` || "01:00"}
+        dropdownStyle={styles.hoursDropdownStyle}
+        onSelect={(index, value) => this.handleHours(id, value)}
+        renderRow={(option) => (
+          <Text style={styles.hoursDropdownOption}>{`0${option}:00`}</Text>
+        )}
+        renderButtonText={(option) => `0${option}:00`}
+      />
+    );
   }
 
   convertObjects = (data) => {
@@ -270,33 +355,29 @@ class MyMapView extends React.Component {
                   ? theme.colors.secondary
                   : markerTheme.COLORS.red
               }
+              // onCalloutPress={() => this.setState({ activeModal: marker })}
+              onCalloutPress={() =>
+                this.scrollToIndex(this.props.markers.indexOf(marker))
+              }
             >
               <Callout tooltip>
-                <TouchableWithoutFeedback
+                <TouchableOpacity
                   onPress={() => this.setState({ active: marker.UUID })}
                 >
-                  <Button
-                    // gradient
-                    style={{ backgroundColor: "transparent" }}
-                    onPress={() => this.setState({ activeModal: item })}
+                  <View
+                    style={[
+                      styles.marker,
+                      styles.shadow,
+                      this.state.active === marker.UUID ? styles.active : null,
+                    ]}
                   >
-                    <View
-                      style={[
-                        styles.marker,
-                        styles.shadow,
-                        this.state.active === marker.UUID
-                          ? styles.active
-                          : null,
-                      ]}
-                    >
-                      <Text style={styles.markerTitle}>{marker.name}</Text>
-                      <Text style={styles.markerStatus}>
-                        {" "}
-                        ({marker.free} / {marker.spots})
-                      </Text>
-                    </View>
-                  </Button>
-                </TouchableWithoutFeedback>
+                    <Text style={styles.markerTitle}>{marker.name}</Text>
+                    <Text style={styles.markerStatus}>
+                      {" "}
+                      ({marker.free} / {marker.spots})
+                    </Text>
+                  </View>
+                </TouchableOpacity>
               </Callout>
             </Marker>
           ))}
@@ -329,9 +410,7 @@ const styles = StyleSheet.create({
     fontWeight: "500",
     paddingVertical: markerTheme.SIZES.base / 3,
   },
-  map: {
-    flex: 3,
-  },
+
   parkings: {
     position: "absolute",
     right: 0,
@@ -360,15 +439,19 @@ const styles = StyleSheet.create({
     paddingRight: markerTheme.SIZES.base,
   },
   buyTotal: {
+    paddingLeft: 8,
     flex: 1,
     justifyContent: "space-evenly",
   },
   buyTotalPrice: {
     color: markerTheme.COLORS.white,
-    fontSize: markerTheme.SIZES.base * 1.85,
+    fontSize: markerTheme.SIZES.base * 2.5,
     fontWeight: "700",
     paddingLeft: markerTheme.SIZES.base * 1.2,
-    // textAlign: "center",
+  },
+  calculation: {
+    color: markerTheme.COLORS.white,
+    fontSize: markerTheme.SIZES.base * 1.7,
   },
   buyBtn: {
     // flex: 0.5,
@@ -404,7 +487,7 @@ const styles = StyleSheet.create({
     shadowRadius: 4,
   },
   active: {
-    borderColor: markerTheme.COLORS.red,
+    borderColor: markerTheme.COLORS.black,
   },
   hours: {
     flex: 1,
@@ -424,15 +507,18 @@ const styles = StyleSheet.create({
     padding: markerTheme.SIZES.base,
     marginRight: markerTheme.SIZES.base / 2,
   },
-  // hoursDropdownOption: {
-  //   padding: 5,
-  //   fontSize: markerTheme.SIZES.font * 0.8,
-  // },
-  // hoursDropdownStyle: {
-  //   marginLeft: -markerTheme.SIZES.base,
-  //   paddingHorizontal: markerTheme.SIZES.base / 2,
-  //   marginVertical: -(markerTheme.SIZES.base + 1),
-  // },
+  hoursDropdownOption: {
+    fontSize: markerTheme.SIZES.font * 0.8,
+    padding: markerTheme.SIZES.base,
+  },
+  hoursDropdownStyle: {
+    borderRadius: markerTheme.SIZES.base / 2,
+    borderColor: markerTheme.COLORS.overlay,
+    borderWidth: 1,
+    marginLeft: -markerTheme.SIZES.base,
+    // paddingHorizontal: markerTheme.SIZES.base / 2,
+    marginVertical: -(markerTheme.SIZES.base + 1),
+  },
   parkingInfoContainer: { flex: 1.5, flexDirection: "row" },
   parkingInfo: {
     justifyContent: "space-evenly",
@@ -466,25 +552,32 @@ const styles = StyleSheet.create({
   modalHours: {
     paddingVertical: height * 0.11,
   },
-  // modalHoursDropdown: {
-  //   flexDirection: "row",
-  //   justifyContent: "center",
-  //   alignItems: "center",
-  //   paddingVertical: markerTheme.SIZES.base,
-  // },
-  // payBtn: {
-  //   borderRadius: 6,
-  //   flexDirection: "row",
-  //   alignItems: "center",
-  //   justifyContent: "space-between",
-  //   padding: markerTheme.SIZES.base * 1.5,
-  //   backgroundColor: markerTheme.COLORS.red,
-  // },
-  // payText: {
-  //   fontWeight: "600",
-  //   fontSize: markerTheme.SIZES.base * 1.5,
-  //   color: markerTheme.COLORS.white,
-  // },
+  modalHoursDropdown: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingVertical: markerTheme.SIZES.base,
+  },
+  payBtn: {
+    borderRadius: 6,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: markerTheme.SIZES.base * 1.5,
+  },
+  payText: {
+    fontWeight: "600",
+    fontSize: markerTheme.SIZES.base * 1.5,
+    color: markerTheme.COLORS.white,
+  },
+  payBtnContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: markerTheme.COLORS.overlay,
+  },
   tableContainer: {
     flex: 1,
     padding: 16,
@@ -492,8 +585,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    borderBottomWidth: 1,
-    borderBottomColor: markerTheme.COLORS.overlay,
+    // borderBottomWidth: 1,
+    // borderBottomColor: markerTheme.COLORS.overlay,
   },
   tableHeader: { height: 50, backgroundColor: theme.colors.primary },
   tableHeaderText: {
@@ -512,4 +605,6 @@ const styles = StyleSheet.create({
   },
   tableDataWrapper: { marginTop: -1 },
   tableRow: { height: 40, flexDirection: "row" },
+
+  markerPrice: { color: markerTheme.COLORS.red, fontWeight: "bold" },
 });
